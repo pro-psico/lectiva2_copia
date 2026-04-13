433f2fab55c1ce143413aaeb5110544b071dc249
Andres <alejoandi@hotmail.com>
2026-04-13 15:03:50 -0500

    Add GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+			base: process.env.GITHUB_PAGES === 'true' ? '/lectiva2_copia' : ''
+		}
 	}
 };
 
